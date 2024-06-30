12cd8ed48217e24bc61b131f970bccf14fb0d0d6
priyansh2120 <priyanshkumar50@gmail.com>
2024-06-30 18:21:29 +0530

    expense tracker backend added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import SignUp from "./components/Signup";
-import Login from "./components/Login";
+
 import Route from "./routes/Routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
