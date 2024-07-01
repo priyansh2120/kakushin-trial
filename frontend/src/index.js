3db5cfb70d986b90398e28fb54bc0d3aeb57ac3f
priyansh2120 <priyanshkumar50@gmail.com>
2024-07-01 22:42:18 +0530

    added vitual currency

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,6 @@ import Route from "./routes/Routes";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     <Route />
   </React.StrictMode>
 );
